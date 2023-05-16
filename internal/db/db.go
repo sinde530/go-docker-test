1f386548256e47c4f3b42a4f5448ddff4f290506
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2023-05-16 17:29:16 +0900

    Create env

--- internal/db/db.go
+++ internal/db/db.go
@@ -3,13 +3,28 @@ package db
 import (
 	"context"
 	"fmt"
+	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+var mongoUrl string
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+		return
+	}
+
+	mongoUrl = os.Getenv("MONGO_URL")
+}
+
 func ConnectToDB() (*mongo.Client, error) {
-	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	clientOptions := options.Client().ApplyURI(mongoUrl)
 
 	client, err := mongo.Connect(context.Background(), clientOptions)
 	if err != nil {
